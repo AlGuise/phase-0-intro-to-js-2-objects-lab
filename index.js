86c9feca7c149431060da8f8b307b6b11a93fae9
Alain Giessel <alain.giessel@gmail.com>
2022-02-24 22:05:42 -0700

    commit tree 7d68eba93637eee8366790d7bacba07f6c28d0c1

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+const employee = {
+    name: "Tom",
+    streetAddress: "1 Privit Drive",
+}
+
+function updateEmployeeWithKeyAndValue(obj, name, streetAddress) {
+    const newEmployee = { ...obj };
+    newEmployee[name] = streetAddress;
+    return newEmployee;
+}
+
+const employeeTwo = updateEmployeeWithKeyAndValue(
+    "Elena",
+    "4 Main Street",
+);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+const replaceEmployee = destructivelyUpdateEmployeeWithKeyAndValue("Karen", "10 Murdock Place")
+
+function deleteFromEmployeeByKey(obj) {
+    const EmployeeTemplate = { ...obj };
+    delete EmployeeTemplate.name
+    delete EmployeeTemplate.streetAddress
+    return EmployeeTemplate
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+const FiredEmployee =  destructivelyDeleteFromEmployeeByKey(key, value)
